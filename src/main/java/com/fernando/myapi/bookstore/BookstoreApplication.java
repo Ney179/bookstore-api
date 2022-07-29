e365b766d6d6b0de385787e8b69e46df08853d6b
Ney179 <fernando.eng.comp2019@gmail.com>
2022-07-29 10:15:38 -0300

    instancia automatica da base de dados

--- src/main/java/com/fernando/myapi/bookstore/BookstoreApplication.java
+++ src/main/java/com/fernando/myapi/bookstore/BookstoreApplication.java
@@ -1,13 +1,40 @@
 package com.fernando.myapi.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fernando.myapi.bookstore.domain.Categoria;
+import com.fernando.myapi.bookstore.domain.Livro;
+import com.fernando.myapi.bookstore.repositories.CategoriaRepository;
+import com.fernando.myapi.bookstore.repositories.LivroRepository;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	LivroRepository livroRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica", "Livro de TI");
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Loren ipsum", cat1);
+		
+		cat1.getLivro().addAll(Arrays.asList(l1));
+		
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
 	}
 
 }

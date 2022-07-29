a8ad2dc6d016bd63a786ba9fafb12496a7374763
Ney179 <fernando.eng.comp2019@gmail.com>
2022-07-29 12:33:28 -0300

    execeção personalizada para objeto não encontrado

--- src/main/java/com/fernando/myapi/bookstore/services/CategoriaService.java
+++ src/main/java/com/fernando/myapi/bookstore/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.fernando.myapi.bookstore.domain.Categoria;
 import com.fernando.myapi.bookstore.repositories.CategoriaRepository;
+import com.fernando.myapi.bookstore.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }

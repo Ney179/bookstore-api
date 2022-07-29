1faa5ae52f47115e5ae56a3ce1ad924680c35df5
Ney179 <fernando.eng.comp2019@gmail.com>
2022-07-29 09:18:50 -0300

    Comite inicial

---
+++ src/test/java/com/fernando/myapi/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.myapi.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
